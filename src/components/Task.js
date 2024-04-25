6d2b8285b5ccddee055b980d2721485bf495409a
Shimpei Ohtani <ohtani.shimpei@gmail.com>
2024-04-25 11:38:33 +0900

    update task component

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,19 +1,25 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
+import { Container, Paper } from '@mui/material';
 
 export default function Task() {
-  return (
-    <Box
-      component="form"
-      sx={{
-        '& > :not(style)': { m: 4, width: '25ch' },
-      }}
-      noValidate
-      autoComplete="off"
-    >
-      <TextField id="outlined-basic" label="Task Name" variant="outlined" />
-      <TextField id="outlined-basic" label="Task URL" variant="outlined" />
-    </Box>
-  );
+    const paperStyle = {padding: "30px 20px", marginTop: "50px"}
+    return (
+        <Container>
+            <Paper elevation={3} style={paperStyle}>
+            <Box
+                component="form"
+                sx={{
+                    '& > :not(style)': { m: 2, width: '25ch' },
+                }}
+                noValidate
+                autoComplete="off"
+            >
+                <TextField id="outlined-basic" label="Task Name" variant="outlined" />
+                <TextField id="outlined-basic" label="Task URL" variant="outlined" />
+            </Box>
+            </Paper>
+        </Container>
+    );
 }
