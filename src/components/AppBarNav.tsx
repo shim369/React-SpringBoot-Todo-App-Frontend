dc2e59f43a4e7acb5dfa4dbf5a08b2c72c43bf01
Shimpei Ohtani <ohtani.shimpei@gmail.com>
2024-04-25 17:25:51 +0900

    delete icon

--- src/components/AppBarNav.tsx
+++ src/components/AppBarNav.tsx
@@ -11,15 +11,6 @@ export default function AppBarNav() {
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             React SpringBoot Todo App
           </Typography>
