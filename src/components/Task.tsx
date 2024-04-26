48d1ad108cf4140fff8922331155b4748effbbbb
Shimpei Ohtani <ohtani.shimpei@gmail.com>
2024-04-26 09:43:01 +0900

    add delete function

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -27,12 +27,37 @@ export default function Task() {
     }
 
     useEffect(() => {
-        fetch("http://localhost:8080/task/getAll")
-            .then(res => res.json())
-            .then((result) => {
-                setTasks(result)
-            })
-    }, [])
+        const getAllTask = () => {
+            fetch("http://localhost:8080/task/getAll")
+                .then(res => res.json())
+                .then((result) => {
+                    setTasks(result)
+                })
+        }
+        getAllTask();
+    }, [tasks])
+
+    const handleDelete = (id: number) => {
+        fetch(`http://localhost:8080/task/delete/${id}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+        })
+        .then(() => {
+            setTasks(tasks.filter((task) => task.id !== id));
+            console.log("Deleted Task");
+        })
+        .catch((error) => {
+            console.error('There was a problem deleting the task:', error);
+        });
+    };
+    
     return (
         <Container>
             <Paper elevation={3} style={paperStyle}>
@@ -60,11 +85,11 @@ export default function Task() {
             <Paper elevation={3} style={paperStyle}>
                 <h2 style={{ color: "#1976d2" }}>Tasks</h2>
                 {tasks.map((task,index) => (
-                    <Paper elevation={3} style={{margin:"10px",padding:"15px",textAlign:"left"}} key={index}>
-                        {task.id} <Link href={task.url} target="_blank">{task.title}</Link>
+                    <Paper elevation={3} style={{margin:"10px",padding:"15px",textAlign:"left", display: 'flex', justifyContent: 'space-between', alignItems: 'center'}} key={index}>
+                        <div>{task.id} <Link href={task.url} target="_blank">{task.title}</Link></div>
+                        <Button variant="contained" onClick={() => handleDelete(task.id)}>Delete</Button>
                     </Paper>
                 ))}
-
             </Paper>
         </Container>
     );
